5608c2d63141b6f86bde8f2f5ae5550cbe213da9
Fabio <fabiosvb@gmail.com>
2017-12-27 13:24:30 -0200

    .

--- src/main/java/funcionalProgramming/thirtyDaysOfCode/Day5_Loops.scala
+++ src/main/java/funcionalProgramming/thirtyDaysOfCode/Day5_Loops.scala
@@ -3,12 +3,9 @@ package funcionalProgramming.thirtyDaysOfCode
 object Day6_LetsReview {
 
     def main(args: Array[String]) {
-        var s = "Hacker"
-        io.Source.stdin.getLines().drop(1).foreach(s => { 
-            print(s.zipWithIndex.filter( (x) => x._2 % 2 == 0).map(_._1).mkString)
-            print(" ")
-            println(s.zipWithIndex.filter( (x) => x._2 % 2 != 0).map(_._1).mkString)
-        })
-    }
+        val sc = new java.util.Scanner(System.in);
+        var n = sc.nextInt();
+        for (i <- 1 to 10) println(n + " x " + i + " = " + (n * i)) 
+    }    
 
-}
+} 
--- src/main/java/funcionalProgramming/thirtyDaysOfCode/Day6_LetsReview.scala
+++ src/main/java/funcionalProgramming/thirtyDaysOfCode/Day6_LetsReview.scala
@@ -3,9 +3,13 @@ package funcionalProgramming.thirtyDaysOfCode
 object Day5_Loops {
 
     def main(args: Array[String]) {
-        val sc = new java.util.Scanner(System.in);
-        var n = sc.nextInt();
-        for (i <- 1 to 10) println(n + " x " + i + " = " + (n * i)) 
+        var s = "Hacker"
+        io.Source.stdin.getLines().drop(1).foreach(s => { 
+            print(s.zipWithIndex.filter( (x) => x._2 % 2 == 0).map(_._1).mkString)
+            print(" ")
+            println(s.zipWithIndex.filter( (x) => x._2 % 2 != 0).map(_._1).mkString)
+        })
     }
+
 
 }
