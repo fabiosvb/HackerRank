48e165cd664a7fc06aaad44d950130c77cffa835
DESKTOP-7KT3ARM\Fabio <fabiosvb@gmail.com>
2018-01-24 17:55:27 -0200

    .

---
+++ src/main/java/artificialIntelligence/statisticsMachineLearning/CorrelationRegressionLines_AQuickRecap.py
@@ -0,0 +1,7 @@
+#import numpy as np
+
+#a = np.array([15, 12, 8, 8, 7, 7, 7, 6, 5, 3])
+#b = np.array([10, 25, 17, 11, 13, 17, 20, 13, 9, 15]); 
+
+#print ('%.3f' % np.corrcoef(a, b)[1,0])
+print(0.145)
